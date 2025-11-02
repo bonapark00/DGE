fa5be41b112c1055794d3fec259e8c9154aaac67
bonapark00 <jaeyeonpark@hcs.snu.ac.kr>
2025-11-02 05:26:22 +0000

    Update scripts and configs

---
+++ script/local_editing/man2clown.sh
@@ -0,0 +1,12 @@
+python launch.py \
+    --config configs/dge_view5.yaml \
+    --train --gpu 2 \
+    trainer.max_steps=1500 \
+    system.prompt_processor.prompt="Turn the man into a clown" \
+    data.source="/working/style-transfer/VcEdit/gs_data/face/" \
+    system.guidance.guidance_scale=12.5 \
+    system.gs_source="/working/style-transfer/VcEdit/gs_data/trained_gs_models/face/point_cloud.ply" \
+    system.seg_prompt="man" \
+    system.target_prompt="clown" \
+    system.mask_thres=0.5
+
