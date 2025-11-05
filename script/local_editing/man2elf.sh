b5d763a41d8023420f563bbb6520c92f331e577a
bonapark00 <jaeyeonpark@hcs.snu.ac.kr>
2025-11-05 05:37:18 +0000

    Add new configuration files and scripts for DGE model training and evaluation
    - Introduced `dge_view5.yaml` for enhanced configuration options.
    - Added various training scripts for different prompts and scenarios, including `man2clown.sh`, `man2elf.sh`, and others.
    - Implemented batch processing capabilities in `man2clown_batch.sh` for efficient GPU utilization.
    - Updated existing scripts to align with the new configuration structure and parameters.

---
+++ script/local_editing/man2elf.sh
@@ -0,0 +1,11 @@
+python launch.py \
+    --config configs/dge_view5.yaml \
+    --train --gpu 2 \
+    trainer.max_steps=1500 \
+    system.prompt_processor.prompt="Turn the man into an elf" \
+    data.source="/working/style-transfer/VcEdit/gs_data/face/" \
+    system.guidance.guidance_scale=12.5 \
+    system.gs_source="/working/style-transfer/VcEdit/gs_data/trained_gs_models/face/point_cloud.ply" \
+    system.seg_prompt="man" \
+    system.mask_thres=0.3
+
