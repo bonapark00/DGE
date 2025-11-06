f43270a495a06e6ab495685083958cf7f4995618
bonapark00 <jaeyeonpark@hcs.snu.ac.kr>
2025-11-06 09:08:24 +0000

    git rm cache

---
+++ script/clip-loss/local_editing/man2elf.sh
@@ -0,0 +1,12 @@
+python launch.py \
+    --config configs/dge_view5.yaml \
+    --train --gpu 6 \
+    trainer.max_steps=1500 \
+    system.prompt_processor.prompt="Turn the man into an elf" \
+    data.source="/working/style-transfer/VcEdit/gs_data/face/" \
+    system.guidance.guidance_scale=12.5 \
+    system.gs_source="/working/style-transfer/VcEdit/gs_data/trained_gs_models/face/point_cloud.ply" \
+    system.seg_prompt="man" \
+    system.target_prompt="elf" \
+    system.mask_thres=0.5 \
+    system.loss.lambda_d=20.0
